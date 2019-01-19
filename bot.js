c96f641067c09c1f426007a356ca2182523336fb
Jacob Young <jwyoung07@gmail.com>
2019-01-19 14:13:44 -0800

    !ping to !test

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 
         args = args.splice(1);
         switch(cmd) {
-            // !ping
+            // !test
             case 'test':
                 bot.sendMessage({
                     to: channelID,
